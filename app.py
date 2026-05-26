1516d0bd7ae1638240e12fa8714907cca3cb9785
Angel-R23 <romuchosotelo5@gmail.com>
2026-05-26 09:51:27 -0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+# Configuración básica de la página
+st.set_page_config(page_title="Prueba H2D", page_icon="🖨️")
+
+# Mensaje de prueba directo en la pantalla
+st.title("🚀 ¡Hola, soy Angel!")
+st.write("Bienvenido, está es la lista para las impresiones. ")
+st.success("Si deseas añadir tu impresión en la cola, escribe al 926 719 339 ")
