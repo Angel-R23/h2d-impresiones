fbf0c2b22187cd2b42ae6f33199c1e39cafc3108
Angel-R23 <romuchosotelo5@gmail.com>
2026-05-26 09:51:04 -0500

    Delete app.py

--- app.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-
-# Configuración básica de la página
-st.set_page_config(page_title="Prueba H2D", page_icon="🖨️")
-
-# Mensaje de prueba directo en la pantalla
-st.title("🚀 ¡Hola, soy Angel!")
-st.write("Si estás viendo este mensaje, significa que tu servidor en Streamlit Cloud está funcionando a la perfección.")
-st.success("¡Conexión exitosa, jefe! El código base corre sin problemas.")
